050ac915e143a620d84509ca7158e0ff93724dc1
Bernadette <tariaobernadette@gmail.com>
2024-11-17 14:32:06 +0800

    changed multiple lines of code

--- Customer.java
+++ Customer.java
@@ -3,13 +3,10 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Customer{
-
-    static Scanner scan = new Scanner(System.in);
+    
     private String fullName;
-    private String currentCustomerName;
     private String email;
     private String contactNo;
-    private String searchCustomer;
 
     public Customer() {
     }
@@ -18,41 +15,6 @@ public class Customer{
         this.fullName = fullName;
         this.email = email;
         this.contactNo = contactNo;
-    }
-    
-    public void enterCustomerInformation() {
-        fullName = validateInput("Customer Full Name");
-        email = validateInput("Email Address");
-        contactNo = validateInput("Contact Number");
-        
-        currentCustomerName = fullName;
-        System.out.println("\n");
-    }
-
-    public void displayCustomerInformation() {
-            if (currentCustomerName != null) {
-                System.out.println("CUSTOMER INFORMATION: ");
-                System.out.print("Customer Name: " + getName() + "\nCustomer Email Address: " + getEmail()
-                        + "\nCustomer Contact Number: " + getContactNo() + "\n\n\n");
-            } else {
-                System.out.println("No customer information found.");
-            }
-        }
-    
-    public String validateInput(String prompt){
-        String input;
-        
-        while(true){
-           System.out.print(prompt + ": ");
-           input = scan.nextLine().trim();
-            
-            if(input.isEmpty()){
-                System.out.println("Invalid input. Please enter valid information");
-            }
-            else{
-                return input;
-            }
-        }
     }
     
     public void setFullName(String fullName) {
